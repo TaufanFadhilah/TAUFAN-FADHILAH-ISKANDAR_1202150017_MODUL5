facb22a935cf069797363eb010da90b4b19d331c
TaufanFadhilah <tfi231097@gmail.com>
2018-03-23 07:55:45 +0700

    create add todo activity

---
+++ app/src/main/java/com/example/android/sqliteprovider/AddTodo.java
@@ -0,0 +1,17 @@
+package com.example.android.sqliteprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddTodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+    }
+
+    public void addTodo(View view) {
+    }
+}
